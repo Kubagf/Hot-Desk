bdeb8c82930c3adaf8e674b02f09d99c47534f03
Jakub Kęsik <kubafg5@gmail.com>
2024-01-25 21:50:18 +0100

    Minor data changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :reservation
 
-  validates :name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password_digest, presence: true
+  validates :role, presence: true
 end
